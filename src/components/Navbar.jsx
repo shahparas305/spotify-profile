622164aff5cb2e7efd77cf3e7c645c4710d9a7e3
shahparas305 <48080182+shahparas305@users.noreply.github.com>
2022-04-22 19:43:00 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import SpotifyIcon from './assets/spotify-icon.png'
 import {Link} from 'react-router-dom'
 import IconUser from './assets/user'
@@ -10,44 +10,60 @@ import {FaGithub} from 'react-icons/fa'
 
 
 function Navbar() {
+  const profile = useRef()
+  const artists = useRef()
+  const tracks = useRef()
+  const recent = useRef()
+  const playlists = useRef()
+
+
+  const changeActiveBtn = (btn) => {
+    removeActiveBtn()
+    if(btn === "profile") profile.current.classList.add('navbar__btn__active')
+    if(btn === "artists") artists.current.classList.add('navbar__btn__active')
+    if(btn === "tracks") tracks.current.classList.add('navbar__btn__active')
+    if(btn === "recent") recent.current.classList.add('navbar__btn__active')
+    if(btn === "playlists") playlists.current.classList.add('navbar__btn__active')
+  }
+
+  const removeActiveBtn = () => {
+    profile.current.classList.remove('navbar__btn__active')
+    artists.current.classList.remove('navbar__btn__active')
+    tracks.current.classList.remove('navbar__btn__active')
+    recent.current.classList.remove('navbar__btn__active')
+    playlists.current.classList.remove('navbar__btn__active')
+  }
+
   return (
     <div className='navbar'>
-        <div className='navbar__logo--c'>
-          <img className='navbar__logo' src={SpotifyIcon}></img>
+        <div class="navbar__logo--c">
+          <img class="navbar__logo" src={SpotifyIcon} alt=''></img>
         </div>
         <div className='navbar__links'>
-            <Link to='/'>
-                <div className='navbar__link__item one'>
-                    <IconUser className="navbar__profile__icon"/>
-                    <div className='navbar__link'>Profile</div>
-                </div>
-            </Link>
-            <Link to='/artists'>
-                <div className='navbar__link__item two'>
-                    <IconMicrophone className="navbar__profile__icon"/>
-                    <div className='navbar__link'>Artists</div>
-                </div>
-            </Link>
-            <Link to='/tracks'>
-                <div className='navbar__link__item three'>
-                    <IconMusic className="navbar__profile__icon"/>
-                    <div className='navbar__link'>Tracks</div>
-                </div>
-              </Link>
-              <Link to='/recent'>
-                <div className='navbar__link__item four'>
-                    <IconTime className="navbar__profile__icon"/>
-                    <div className='navbar__link'>Recent</div>
-                </div>
-              </Link>
-              <Link to='/playlists'>
-                <div className='navbar__link__item five'>
-                    <IconPlaylist className="navbar__profile__icon"/>
-                    <div className='navbar__link'>Playlists</div>
-                </div>
-              </Link>   
+          <Link to='/'  ref={profile} class="navbar__link__item navbar__profile navbar__btn__active"  onClick={() => changeActiveBtn('profile')}>
+            <IconUser className="navbar__profile__icon"/>
+            <div className='navbar__icon__title'>Profile</div>
+          </Link>
+          <Link to='/artists'  ref={artists} class="navbar__link__item navbar__artists" onClick={() => changeActiveBtn('artists')}>
+            <IconMicrophone className="navbar__profile__icon"/>
+            <div className='navbar__icon__title'>Artists</div>
+          </Link>    
+          <Link to='/tracks'  ref={tracks} class="navbar__link__item navbar__tracks" onClick={() => changeActiveBtn('tracks')}>
+            <IconMusic className="navbar__profile__icon"/>
+            <div className='navbar__icon__title'>Tracks</div>
+          </Link>   
+          <Link to='/recent'  ref={recent} class="navbar__link__item navbar__recent" onClick={() => changeActiveBtn('recent')}>
+            <IconTime className="navbar__profile__icon"/>
+            <div className='navbar__icon__title'>Recent</div>
+          </Link>    
+          <Link to='/playlists'  ref={playlists} class="navbar__link__item navbar__playlists" onClick={() => changeActiveBtn('playlists')}>
+            <IconPlaylist className="navbar__profile__icon"/>
+            <div className='navbar__icon__title'>Playlists</div>
+          </Link>
         </div>
-        <FaGithub className='navbar__github__logo'/>
+        <div className='navbar__github__logo--c'>
+          <FaGithub className='navbar__github__logo'/>
+        </div>
     </div>
   )
 }
