66e211063e5b7bf3d9ff98b61fbc3ada7cc5fb7b
shahparas305 <48080182+shahparas305@users.noreply.github.com>
2022-04-22 21:06:24 -0400

    Update TrackList.jsx

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -4,98 +4,9 @@ import Track from './Track'
 
 function TrackList({songs}) {
 
-//     const tracks = [
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 1
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 2
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 3
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 4
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 5
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 6
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 7
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 8
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 9
-//     },
-//     {
-//         image: beatbox,
-//         title: 'Beat Box',
-//         artist: 'SpotemGottem',
-//         album: 'Beat Box',
-//         length: '1:50',
-//         id: 10
-//     },
-// ]
-
-
   return (
-    // <div>
-    //     {tracks.map((track) => (
-    //         <Track key={track.id} track={track}/>
-    //     ))}
-    // </div>
 
-    <div>
+    <div className="tracklist5">
         {songs.map((track) => (
             <Track key={track.id} track={track}/>
         ))}
